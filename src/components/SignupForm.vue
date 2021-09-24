6220afd74902fbb49b4c0b396850ec2fa8c18ae3
Anthony <anthony.gilbert93@gmail.com>
2021-09-23 19:01:20 -0500

    106. Creating a Signup Composable

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -14,14 +14,20 @@
 
 <script>
 import { ref } from "@vue/reactivity";
+import useSignup from "@/composables/useSignup.js";
+
 export default {
   setup() {
     const displayName = ref("");
     const email = ref("");
     const password = ref("");
 
-    const handleSubmit = () => {
+    const { error, signup } = useSignup();
+
+    const handleSubmit = async () => {
       console.log(displayName.value, email.value, password.value);
+      await signup(email.value, password.value, displayName.value);
+      console.log("User signed up");
     };
 
     return { displayName, email, password, handleSubmit };
