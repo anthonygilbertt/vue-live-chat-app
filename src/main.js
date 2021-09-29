f5ce514e8a5ceea515d9191d84ee411394d71be0
Anthony <anthony.gilbert93@gmail.com>
2021-09-29 18:52:04 -0500

    114. Waiting for Firebase Auth to Initialize

--- src/main.js
+++ src/main.js
@@ -3,5 +3,17 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import { projectAuth } from './firebase/config'
 
-createApp(App).use(router).mount('#app')
+let app
+
+projectAuth.onAuthStateChanged(() => {
+    // this gets fired when the Auth State changes
+    // for example: when when we "have a user"
+    // (The user state changes)
+    if (!app) {
+        app = createApp(App)
+            .use(router)
+            .mount('#app') //we don't want to mount until the auth state changes 
+    }
+})
