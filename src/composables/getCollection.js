8da78a082fb80b1587124093122df2bee2192775
Anthony <anthony.gilbert93@gmail.com>
2021-09-30 18:37:15 -0500

    118. Real-Time FireStore listener(composable)

---
+++ src/composables/getCollection.js
@@ -0,0 +1,41 @@
+import { ref } from "@vue/reactivity";
+import { projectFirestore } from '../firebase/config'
+
+
+const getCollection = (collection) => {
+    let documents = ref(null)
+    let error = ref(null)
+
+    const getDoc = async (doc) => {
+        error.value = null
+
+
+        // await projectFirestore.collection(collection).get(doc)
+        let collectionRef = projectFirestore.collection(collection)
+            .orderBy('createdAt')
+
+        collectionRef.onSnapshot(snap => {
+            let results = [] //we're going to have more than 1 result,
+            // so we're creating an empty array
+
+            // now we want to cycle through the 'snap' and
+            //add each result to the newly created array
+            snap.docs.forEach((doc) => {
+                // this returns all the results of the document + the id for the document
+
+                doc.data().createdAt && results.push({ ...doc.data(), id: doc.id })
+            })
+            documents.value = results
+            error.value = null
+
+        }, (err) => {
+            console.log(err.message);
+            documents.value = null
+        }) //this is how you set up a real time listener
+
+    }
+
+    return { documents, error }
+}
+
+export default getCollection
