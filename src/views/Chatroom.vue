95fad0db82e90e389ad89d188e34d41effd5db68
Anthony <anthony.gilbert93@gmail.com>
2021-09-29 19:20:02 -0500

    115. CHALLENGE- Watching the current user

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -7,9 +7,26 @@
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import getUser from "../composables/getUser";
+import { useRouter } from "vue-router";
+import { watch } from "@vue/runtime-core";
 
 export default {
   components: { Navbar },
+  setup() {
+    //get the current user using the getUser composable
+    const { user } = getUser();
+    const router = useRouter();
+
+    // Watch the user for changes
+    watch(user, (user, prevUser) => {
+      router.push({ name: "Welcome" });
+    });
+
+    //Redirect the user to the Welcome page when the user logs out
+
+    return { user };
+  },
 };
 </script>
 
