47ec12fd1cc8246f4193a1cae7db6b1403463af3
Anthony <anthony.gilbert93@gmail.com>
2021-10-05 19:23:43 -0500

    123. Final Route Guard

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,26 @@ import { projectAuth } from '../firebase/config'
 const routes = [{
     path: '/',
     name: 'Welcome',
-    component: Welcome
+    component: Welcome,
+    beforeEnter: (to, from, next) => {
+        // to:  the component of the route that we are going to
+        // from: the route we are coming from
+        // next:  CB function- Allows user to access the route
+        //          or redirects them
+
+        let user = projectAuth.currentUser
+
+        if (user) {
+            next({ name: 'Chatroom' }) //this allows the user access
+        } else {
+
+            next() //this allows the user access
+        }
+        /*
+        Now, we register the auth guard inside of a route,
+            we are going to go with /chatroom
+        */
+    }
 },
 {
     path: '/chatroom',
